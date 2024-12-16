ecfada9dc5d7a141096158b97e635a31b5503233
Guzel <viproo@bk.ru>
2024-12-16 15:44:35 +0300

    hw1

---
+++ src/components/OptionsInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="input-wrapper">Options Input: {{ inputValue }}</div>
+  <input @input="inputValueChange" :value="inputValue" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'OptionsInput',
+  data() {
+    return {
+      inputValue: '',
+    };
+  },
+  methods: {
+    inputValueChange($event) {
+      this.inputValue = $event.target.value;
+    },
+  },
+});
+</script>
+
+<style scoped>
+.input-wrapper {
+  font-size: 30px;
+  color: green;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import CompositionInput from '../components/CompositionInput.vue';
+import OptionsInput from '../components/OptionsInput.vue';
+</script>
+
+<template>
+  <main>
+    <CompositionInput />
+    <OptionsInput />
+  </main>
+</template>
